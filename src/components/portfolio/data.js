8d74f6ecc1b89aab80c7178de57f66243cf93223
Dixit Gogoi <dixitgogoi2017@gmail.com>
2023-03-27 10:40:08 +0530

    Portfolio section updated

--- src/components/portfolio/data.js
+++ src/components/portfolio/data.js
@@ -23,31 +23,31 @@ const data = [
     {
       id: 3,
       image: IMG3,
-      title: "Okacoin Logo Design",
-      github: "https://github.com",
-      demo: "Jalaga - Shipping Automation Software Website Design",
+      title: "Portfolio",
+      github: "https://github.com/medixitgogoi/portfolio",
+      demo: "https://portfolio-nine-navy-48.vercel.app/",
     },
     {
       id: 4,
       image: IMG4,
-      title: "Snowflake knotwork",
+      title: "StreamRay",
       github: "https://github.com/medixitgogoi/StreamRay",
       demo: "https://stream-ray.vercel.app/",
     },
-    {
-      id: 5,
-      image: IMG5,
-      title: "Shipping Management System",
-      github: "https://github.com",
-      demo: "https://dribbble.com/shots/19995095-Shipping-Management-System",
-    },
-    {
-      id: 6,
-      image: IMG6,
-      title: "tech consultant logo and branding design - tech support logo",
-      github: "https://github.com",
-      demo: "https://dribbble.com/shots/19970434-tech-consultant-logo-and-branding-design-tech-support-logo",
-    },
+    // {
+    //   id: 5,
+    //   image: IMG5,
+    //   title: "Shipping Management System",
+    //   github: "https://github.com",
+    //   demo: "https://dribbble.com/shots/19995095-Shipping-Management-System",
+    // },
+    // {
+    //   id: 6,
+    //   image: IMG6,
+    //   title: "tech consultant logo and branding design - tech support logo",
+    //   github: "https://github.com",
+    //   demo: "https://dribbble.com/shots/19970434-tech-consultant-logo-and-branding-design-tech-support-logo",
+    // },
   ];
 
   export default data
