2a33e8ec11c5d3fbf1a07cdb6fca58e3210c9e1c
Dixit Gogoi <dixitgogoi2017@gmail.com>
2023-08-16 18:51:19 +0530

    Footer updated with animations

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,36 +3,87 @@ import "./footer.css"
 import { BsLinkedin } from "react-icons/bs";
 import { FaGithub } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
+import { motion } from 'framer-motion';
 
 const Footer = () => {
     return (
-        <footer id="footer">
-            <a href="#" className="footer__logo">DIXIT GOGOI</a>
+        <motion.footer id="footer"
+            initial={{ opacity: 0, translateX: -400 }}
+            whileInView={{ opacity: 1, translateX: 0 }}
+            transition={{duration: 0.5}}
+        >
+            <motion.a href="#" className="footer__logo"
+                initial={{ opacity: 0, scale: 2 }}
+                whileInView={{ opacity: 1, scale: 1 }}
+                transition={{ duration: 0.5 }}
+            >DIXIT GOGOI</motion.a>
 
-            <ul className="permalinks">
-                <li><a href="#">Home</a><span>|</span></li>
-                <li><a href="#about">About</a><span>|</span></li>
-                <li><a href="#contact">Contact</a><span>|</span></li>
-                <li><a href="#experience">Experience</a><span>|</span></li>
-                <li><a href="#portfolio">Portfolio</a></li>
+            <ul className="permalinks"
+            >
+                <motion.li
+                    initial={{ opacity: 0, translateY: -100 }}
+                    whileInView={{ opacity: 1, translateY: 0 }}
+                    transition={{ duration: 0.5 }}
+                ><a href="#">Home</a><span>|</span></motion.li>
+
+                <motion.li
+                    initial={{ opacity: 0, translateY: 100 }}
+                    whileInView={{ opacity: 1, translateY: 0 }}
+                    transition={{ duration: 0.5 }}
+                ><a href="#about">About</a><span>|</span></motion.li>
+
+                <motion.li
+                    initial={{ opacity: 0, translateY: -100 }}
+                    whileInView={{ opacity: 1, translateY: 0 }}
+                    transition={{ duration: 0.5 }}
+                ><a href="#contact">Contact</a><span>|</span></motion.li>
+                
+                <motion.li
+                    initial={{ opacity: 0, translateY: 100 }}
+                    whileInView={{ opacity: 1, translateY: 0 }}
+                    transition={{ duration: 0.5 }}
+                ><a href="#experience">Experience</a><span>|</span></motion.li>
+                
+                <motion.li
+                    initial={{ opacity: 0, translateY: -100 }}
+                    whileInView={{ opacity: 1, translateY: 0 }}
+                    transition={{ duration: 0.5 }}
+                ><a href="#portfolio">Portfolio</a></motion.li>
             </ul>
 
             <div className="footer__socials">
-                <a href="https://www.linkedin.com/in/dixit-gogoi-9076981b2/" target="_blank">
+                
+                <motion.a href="https://www.linkedin.com/in/dixit-gogoi-9076981b2/" target="_blank"
+                    initial={{ opacity: 0, translateY: -50 }}
+                    whileInView={{ opacity: 1, translateY: 0 }}
+                    transition={{ duration: 0.5 }}
+                >
                     <BsLinkedin />
-                </a>
-                <a href="https://github.com/medixitgogoi" target="_blank">
+                </motion.a>
+
+                <motion.a href="https://github.com/medixitgogoi" target="_blank"
+                    initial={{ opacity: 0, translateY: 50 }}
+                    whileInView={{ opacity: 1, translateY: 0 }}
+                    transition={{ duration: 0.5 }}
+                >
                     <FaGithub />
-                </a>
-                <a href="https://twitter.com/kargilDixit_32" target="_blank">
+                </motion.a>
+
+                <motion.a href="https://twitter.com/kargilDixit_32" target="_blank"
+                    initial={{ opacity: 0, translateY: -50 }}
+                    whileInView={{ opacity: 1, translateY: 0 }}
+                    transition={{ duration: 0.5 }}
+                >
                     <FaTwitter />
-                </a>
+                </motion.a>
+
             </div>
 
             <div className="footer__copyright">
                 <small>&copy; DIXIT All rights reserved.</small>
             </div>
-        </footer>
+
+        </motion.footer>
     )
 }
 
