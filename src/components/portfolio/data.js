b1fd24cfa1afbbdad2f25a54042dadbfaddbc250
Dixit Gogoi <dixitgogoi2017@gmail.com>
2023-12-20 18:24:52 +0530

    portfolio updated

--- src/components/portfolio/data.js
+++ src/components/portfolio/data.js
@@ -3,6 +3,7 @@ import IMG3 from "../../assets/portfolio2.png";
 import IMG4 from "../../assets/portfolio3.png";
 import IMG5 from "../../assets/portfolio4.png";
 import IMG2 from "../../assets/portfolio1.png";
+import IMG6 from "../../assets/portfolio6.png";
 
 const data = [
   {
@@ -40,32 +41,13 @@ const data = [
     github: "https://github.com/medixitgogoi/Restaurant-app",
     demo: "https://saffron-spice.vercel.app/",
   },
-  // {
-  //   id: 6,
-  //   image: IMG6,
-  //   title: "tech consultant logo and branding design - tech support logo",
-  //   github: "https://github.com",
-  //   demo: "https://dribbble.com/shots/19970434-tech-consultant-logo-and-branding-design-tech-support-logo",
-  // },
+  {
+    id: 6,
+    image: IMG6,
+    title: "StreamRay",
+    github: "https://github.com/medixitgogoi/StreamRay",
+    demo: "https://stream-ray.vercel.app/",
+  },
 ];
 
 export default data;
-
-// {
-//   id: 2,
-//     image: IMG2,
-//       title: "TokenTradeX",
-//         github: "https://github.com/medixitgogoi/TokenTradeX",
-//           demo: "https://token-trade-x.vercel.app/",
-//   },
-
-// {
-//   id: 3,
-//     image: IMG3,
-//       title: "SaffronSpice",
-//         github: "https://github.com/medixitgogoi/Restaurant-app",
-//           demo: "https://saffron-spice.vercel.app/",
-//   },
-
-
-
